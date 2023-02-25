0dede0dcb12357d680d10cd0b9a9d55dad373c13
dribe <bekizepp@gmail.com>
2023-02-25 06:26:40 +0100

    first commit

---
+++ lnr.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+PATTERN=$(date +"%m,%d")
+FILE=~/Code/lnr/lnr.csv
+
+
+a_3=$(grep $PATTERN $FILE | awk '{split($0,times,","); print times[3]}')
+a_4=$(grep $PATTERN $FILE | awk '{split($0,times,","); print times[4]}')
+a_5=$(grep $PATTERN $FILE | awk '{split($0,times,","); print times[5]}')
+a_6=$(grep $PATTERN $FILE | awk '{split($0,times,","); print times[6]}')
+a_7=$(grep $PATTERN $FILE | awk '{split($0,times,","); print times[7]}')
+a_8=$(grep $PATTERN $FILE | awk '{split($0,times,","); print times[8]}')
+
+print_times() {
+    echo "Imsaku: $a_3"
+    echo "Sabahu: $a_4"
+    echo "Yleja: $a_5"
+    echo "Ikindia: $a_6"
+    echo "Akshami: $a_7"
+    echo "Jacia: $a_8"
+}
+
+if [ $# -eq 0 ]; then
+    print_times
+else
+    case $1 in
+        imsak)      echo "Imsaku: $a_3";;
+        sabah)      echo "Sabahu: $a_4";;
+        yle)        echo "Yleja: $a_5";;
+        ikindi)     echo "Ikindia: $a_6";;
+        aksham)     echo "Akshami: $a_7";;
+        jaci)       echo "Jacia: $a_8";;
+        *)          print_times;;
+    esac
+fi
