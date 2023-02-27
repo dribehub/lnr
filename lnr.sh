d78233b85e034f186b2e5a1556b0e02bd394a103
dribe <bekizepp@gmail.com>
2023-02-27 22:30:44 +0100

    more refactoring

--- lnr.sh
+++ lnr.sh
@@ -2,32 +2,27 @@
 
 PATTERN=$(date +"%m,%d")
 FILE=~/Code/lnr/lnr.csv
-
 TODAY=$(grep $PATTERN $FILE)
 
 echo_prayer_time() {
     echo $(echo $TODAY | awk '{split($0,times,","); print times['$(($1+3))']}')
 }
 
-prayer_times=(
-    $(echo_prayer_time 0)
-    $(echo_prayer_time 1)
-    $(echo_prayer_time 2)
-    $(echo_prayer_time 3)
-    $(echo_prayer_time 4)
-    $(echo_prayer_time 5)
-)
-
 print_times() {
     case $1 in
-        imsak|0)    echo "Imsaku: ${prayer_times[0]}";;
-        sabah|1)    echo "Sabahu: ${prayer_times[1]}";;
-        yle|2)      echo "Yleja: ${prayer_times[2]}";;
-        ikindi|3)   echo "Ikindia: ${prayer_times[3]}";;
-        aksham|4)   echo "Akshami: ${prayer_times[4]}";;
-        jaci|5)     echo "Jacia: ${prayer_times[5]}";;
-        *)          print_times 0; print_times 1; print_times 2; print_times 3; print_times 4; print_times 5;;
+        0) echo "Imsaku: $(echo_prayer_time 0)";;
+        1) echo "Sabahu: $(echo_prayer_time 1)";;
+        2) echo "Yleja: $(echo_prayer_time 2)";;
+        3) echo "Ikindia: $(echo_prayer_time 3)";;
+        4) echo "Akshami: $(echo_prayer_time 4)";;
+        5) echo "Jacia: $(echo_prayer_time 5)";;
+        *) for i in {0..5}; do print_times $i; done;;
     esac
 }
 
-[ $# -eq 0 ] && print_times all || print_times $1
+[ $# -eq 0 ] && # if no argument is used
+    print_times all || # print all praying times
+    print_times $1 # else print the indexed praying time
+
+# TODO:
+    # add default time based on current time
